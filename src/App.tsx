6cf04d4e566454432eaa3814ab87a5de654b8f7f
Rafael Colombo <92228307+Rafael-OliveiraC@users.noreply.github.com>
2022-08-11 21:12:02 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Sect1 from "./components/section1/Sect1"
+
+import './app.scss'
+
+function App() {
+
+
+  return (
+    <div>
+      <Sect1 />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/section2/Sect2.tsx
@@ -0,0 +1,7 @@
+export default function section2(){
+    return(
+        <div>
+            <h1>Section 1</h1>
+        </div>
+    )
+}
